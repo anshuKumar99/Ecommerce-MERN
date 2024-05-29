9721ce3a3743b77da885c6727a8ef2ac7fd16223
Anshu Kumar <anshu.kumar2017@vitalum.ac.in>
2024-05-30 01:00:05 +0530

    Add files via upload

---
+++ Myntra-Clone-React/src/Components/FetchItems.jsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchStatusActions } from "../store/fetchStatusSlice";
+import { itemsActions } from "../store/itemsSlice";
+
+//Headless Component : Used for storing logic
+const FetchItems = () => {
+  const fetchStatus = useSelector((store) => store.fetchStatus);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (fetchStatus.fetchDone) {
+      return;
+    }
+    const controller = new AbortController();
+    const signal = controller.signal;
+    dispatch(fetchStatusActions.markFetchingStarted());
+    fetch("http://localhost:8080/items", { signal })
+      .then((response) => response.json())
+      .then(({ items }) => {
+        dispatch(fetchStatusActions.markFetchDone());
+        dispatch(fetchStatusActions.markFetchingFinished());
+        dispatch(itemsActions.addInitialItems(items[0]));
+      });
+
+    return () => {
+      controller.abort();
+    };
+  }, [fetchStatus]);
+  return <></>;
+};
+
+export default FetchItems;
